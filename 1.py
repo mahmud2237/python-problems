a8d8e3e1be3ee329f34e5c39ed0430a02317d8bf
mahmud2237 <smahmud2237@gmail.com>
2024-04-05 23:04:20 +0600

    First problem solutions

---
+++ 1.py
@@ -0,0 +1,32 @@
+"""
+Easy
+Problem Description
+Write a program to assign one variable to another.
+
+Create a variable named favorite_food with the value "steak".
+Print the favorite_food variable.
+Create another variable named food with the value "pizza".
+Assign the food variable to the favorite_food variable.
+Print the favorite_food variable again.
+
+
+Example
+Expected Output:
+steak
+pizza
+"""
+
+# create the favorite_food variable
+favorite_food = "steak"
+
+# print the favorite_food variable 
+print(favorite_food)
+
+# create the food variable
+food = "pizza"
+
+# assign food to favorite_food
+favorite_food = food
+
+# print the favorite_food variable
+print(favorite_food)
