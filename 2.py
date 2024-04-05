68bc35da1acb35c66972acd12e5ff9053298f173
mahmud2237 <smahmud2237@gmail.com>
2024-04-05 23:12:54 +0600

    2nd problem solutions done

---
+++ 2.py
@@ -0,0 +1,22 @@
+#Print String and a Number
+"""
+Easy
+Problem Description
+Write a program to print a string and a number using a single print() statement.
+
+Create a variable age and use it to store the value 19.
+Create another variable message to store the string "The age stored in the variable is:".
+Use a single print() statement to display the message and age variables together.
+Example
+Expected Output
+
+The age stored in the variable is: 19
+"""
+# create a variable age 
+age = 19
+
+# create message variable
+message = "The age stored in the variable is:"
+
+# print message and age in the same line
+print(message, age)
