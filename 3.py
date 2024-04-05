2a290053b901c8201018c33f94ec9ace083a8bd1
mahmud2237 <smahmud2237@gmail.com>
2024-04-05 23:19:39 +0600

    3rd problem solutions done

---
+++ 3.py
@@ -0,0 +1,13 @@
+# Newline
+'''
+Easy
+Problem Description
+Write a program to display Hey and How are you in two different lines using a single print() statement.
+
+Example
+Expected Output
+
+Hey
+How are you
+'''
+print("Hey\nHow are you")
