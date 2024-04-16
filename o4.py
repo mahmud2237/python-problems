c87bfa0543123ae75b02a3088e072203907fc1de
mahmud2237 <smahmud2237@gmail.com>
2024-04-16 16:23:40 +0600

    Operation problem 4

---
+++ o4.py
@@ -0,0 +1,29 @@
+'''
+Write a program to print the largest number of the two numbers given.
+
+Input:-
+a=3, b=4
+
+Output:
+4
+'''
+
+#========== User's Code Starts Here ==========
+def find_max(a,b):
+    """ 
+    """
+    if a>b:
+        return a
+    else:
+        return b
+    
+    
+    
+    """Dont change anything below. If changed click on reset."""
+def main():
+    a = int(input())
+    b = int(input())
+    print(find_max(a,b))
+    
+main()
+#========== User's Code Ends Here ==========
